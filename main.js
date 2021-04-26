4df1e88d50908e7832f7be2117776d5ad394a9ba
tjf <tjf@megvii.com>
2021-04-26 20:14:37 +0800

    [add]: add tary icon.

--- main.js
+++ main.js
@@ -1,33 +1,41 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, Tray } = require('electron')
 const path = require('path')
 
-function createWindow () {
-      const win = new BrowserWindow({
-              width: 800,
-              height: 600,
-              webPreferences: {
-                        preload: path.join(__dirname, 'preload.js')
-                      }
-            })
+let tray = null
 
-      win.loadFile('index.html')
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
 }
 
-
-
 app.whenReady().then(() => {
-      createWindow()
+  tray = new Tray('assets/icons/icon.ico')
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Item1', type: 'radio' },
+    { label: 'Item2', type: 'radio' },
+    { label: 'Item3', type: 'radio', checked: true },
+    { label: 'Item4', type: 'radio' }
+  ])
+  tray.setToolTip('This is my application.')
+  tray.setContextMenu(contextMenu)
+  createWindow()
 
-      app.on('activate', () => {
-              if (BrowserWindow.getAllWindows().length === 0) {
-                        createWindow()
-                      }
-            })
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
 })
 
 app.on('window-all-closed', () => {
-      if (process.platform !== 'darwin') {
-              app.quit()
-            }
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
-
